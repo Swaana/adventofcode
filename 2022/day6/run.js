5a448e2e14563b126cf651bc939c1c0cd9e7a3fc
Swaana <swaanas@gmail.com>
2022-12-06 06:55:21 +0100

    day6

---
+++ 2022/day6/run.js
@@ -0,0 +1,26 @@
+const readInput = require('../utils/readInput')
+
+const input = readInput(__dirname, 'input.txt')
+const tests = input.split('\n')
+
+function hasDuplicateChars (value) {
+  const charArray = [value[0]]
+  for (let i = 1; i < value.length; i++) {
+    if (charArray.includes(value[i])) {
+      return true;
+    }
+    charArray.push(value[i]);
+  }
+  return false;
+}
+
+function getStartOfPacketIndex (data, nrOfDistinct) {
+  for (let i = 0; i < data.length - nrOfDistinct; i++) {
+    if (!hasDuplicateChars(data.substring(i, i + nrOfDistinct))) {
+      return i + nrOfDistinct;
+    }
+  }
+}
+
+console.log('part1', getStartOfPacketIndex(tests[0], 4))
+console.log('part2', getStartOfPacketIndex(tests[0], 14))
