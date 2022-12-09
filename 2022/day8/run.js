06505e81eb9ffa2a63e4df771b642191781c7261
Swaana Snorradottir <swaana.snorradottir1@nn.nl>
2022-12-09 07:51:32 +0100

    day8

--- 2022/day8/run.js
+++ 2022/day8/run.js
@@ -6,7 +6,7 @@ lines.pop();
 
 const trees = lines.map(l => [...l].map(Number))
 
-console.log(trees)
+// console.log(trees)
 
 function isVisibleFromLeftOrRight (tree, treeIndex, treeLine) {
   const maxLeft = Math.max(...treeLine.slice(0, treeIndex));
@@ -46,3 +46,60 @@ trees.forEach((treeLine, index) => {
 
 // console.log(visibleTrees)
 console.log('part1', visibleTrees.reduce((sum, t) => sum += t.reduce((sum, v) => sum += v), 0))
+
+function getVisibleTrees (tree, list) {
+  let count = 0
+  for(let i = 0; i < list.length; i++) {
+    count ++;
+    // console.log(tree, '>=', list[i], list)
+    if (tree <= list[i]) {
+      return count;
+    }
+  }
+  return count;
+}
+function getScenicValue (x, y, trees) {
+  const tree = trees[x][y]
+  const horizontalRow = trees[x];
+  const verticalRow = trees.map(t => t[y]);
+
+  const lines = [
+    horizontalRow.slice(0, y),
+    horizontalRow.slice(y + 1),
+    verticalRow.slice(0, x),
+    verticalRow.slice(x + 1)
+  ]
+
+  const visible = [
+    getVisibleTrees(tree, [...lines[0]].reverse()),
+    getVisibleTrees(tree, lines[1]),
+    getVisibleTrees(tree, [...lines[2]].reverse()),
+    getVisibleTrees(tree, lines[3]),
+  ]
+  // console.log(lines, tree, visible)
+  return visible.reduce((s, v) => s*v, 1)
+}
+
+const scenicTrees = []
+let maxValue = 0;
+trees.forEach((treeLine, index) => {
+  if (index === 0 || index === trees.length - 1) {
+    scenicTrees.push(treeLine.map(() => 0));
+    return;
+  }
+  const scenicLine = []
+  treeLine.forEach((tree, treeIndex) => {
+    if (treeIndex === 0 || treeIndex === treeLine.length - 1) {
+      scenicLine.push(0);
+      return;
+    }
+    const value = getScenicValue(index, treeIndex, trees)
+    if (value > maxValue) {
+      maxValue = value;
+    }
+    scenicLine.push(value);
+  })
+  scenicTrees.push(scenicLine)
+})
+
+console.log('part2', maxValue)
