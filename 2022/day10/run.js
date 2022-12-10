8d0f2c7534363ad44fe9165af6ab014ff2a5fac5
Swaana <swaanas@gmail.com>
2022-12-10 09:38:37 +0100

    day10

---
+++ 2022/day10/run.js
@@ -0,0 +1,55 @@
+const readInput = require('../utils/readInput')
+
+const input = readInput(__dirname, 'input.txt')
+const lines = input.split('\n')
+lines.pop()
+
+const commands = lines.map(l => l.split(' ').map((p, i) => i > 0 ? Number(p) : p))
+
+const cmds = {
+  noop: 'noop',
+  addx: 'addx',
+}
+
+let x = 1
+let cycleNr = 0
+const xValues = []
+const signalValues = []
+let line = []
+const screen = []
+
+
+function runCycleTick () {
+  xValues.push(x);
+  cycleNr++;
+
+  const pixel = cycleNr % 40 - 1;
+  if (pixel >= x - 1 && pixel <= x + 1) {
+    line.push('#');
+  } else {
+    line.push(' ');
+  }
+
+  if (cycleNr % 40 === 0) {
+    screen.push(line);
+    line = [];
+  }
+
+  if ((cycleNr - 20) % 40 === 0) {
+    signalValues.push(x * cycleNr);
+  }
+}
+
+commands.forEach(([cmd, amount]) => {
+  if (cmd === cmds.noop) {
+    runCycleTick()
+  } else if (cmd === cmds.addx) {
+    runCycleTick()
+    runCycleTick()
+    x += amount
+  }
+})
+
+console.log('part1', signalValues.reduce((sum, v) => sum + v, 0));
+console.log('part2');
+console.log(screen.map(l => l.join('')).join('\n'));
