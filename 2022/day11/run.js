b99918ce88fc4f070038434b39655fc00bd7f751
Swaana <swaanas@gmail.com>
2022-12-11 22:25:28 +0100

    day11

--- 2022/day11/run.js
+++ 2022/day11/run.js
@@ -1,66 +1,65 @@
 const readInput = require('../utils/readInput')
 const { sortByNumberDesc } = require('../utils/sortUtils')
-const start = process.hrtime();
-const input = readInput(__dirname, 'inputTst.txt')
+const input = readInput(__dirname, 'input.txt')
 
 const operator = {
   '+': (a, b) => a + b,
   '*': (a, b) => a * b,
+  '^2': (a) => a * a,
 }
 
-const monkeys = input
+const monkeysInput = input
   .split('\n\n')
   .map((m) => {
     const lines = m.split('\n')
-    const items = lines[1].substring(18).split(', ').map(BigInt)
+    const items = lines[1].substring(18).split(', ').map(Number)
 
-    const worryLevelParts = lines[2].substring(13).split(' ');
-    const sign = worryLevelParts[3];
-    const b = isNaN(worryLevelParts[4]) ? false : BigInt(worryLevelParts[4]);
+    const worryLevelParts = lines[2].substring(13).split(' ')
+    const sign = worryLevelParts[4] === 'old' ? '^2' : worryLevelParts[3]
+    const b = Number(worryLevelParts[4])
 
-    // part1
-    // const getWorryLevel = (v) => operator[sign](v, b || v) / 3n;
-
-    //part 2
-    const getWorryLevel = (v) => operator[sign](v, b || v);
-
-    const mod = BigInt(lines[3].split(' ').pop())
+    const testValue = Number(lines[3].split(' ').pop())
     const trueMonkey = Number(lines[4].split(' ').pop())
     const falseMonkey = Number(lines[5].split(' ').pop())
-    return { items, getWorryLevel, mod, trueMonkey, falseMonkey, itemsInspected: 0 }
-  });
-// console.log(monkeys)
-function monkeyTurn (monkey, index) {
+
+    return { items, sign, b, testValue, trueMonkey, falseMonkey, itemsInspected: 0 }
+  })
+
+const denominator = monkeysInput.map(m => m.testValue).reduce((a, b) => a * b)
+
+let monkeys
+
+function monkeyTurn (monkey) {
   while (monkey.items.length > 0) {
-    const currentItem = monkey.items.shift();
-    const worryLevel = monkey.getWorryLevel(currentItem);
-    monkey.itemsInspected++;
-    const nextMonkey = worryLevel % monkey.mod === 0n ? monkey.trueMonkey : monkey.falseMonkey;
+    const currentItem = monkey.items.shift()
+    const worryLevel = monkey.getWorryLevel(currentItem) % denominator;
+    const nextMonkey = worryLevel % monkey.testValue === 0 ? monkey.trueMonkey : monkey.falseMonkey
     monkeys[nextMonkey].items.push(worryLevel)
+
+    monkey.itemsInspected++
   }
 }
 
-let roundNr = 0;
-function runRound () {
-  roundNr++;
-  monkeys.forEach(monkeyTurn);
-}
+function runRounds (x, hasRelief, isLog) {
+  monkeys = JSON.parse(JSON.stringify(monkeysInput)).map(m => ({
+    ...m,
+    getWorryLevel: hasRelief
+      ? (v) => Math.floor(operator[m.sign](v, m.b) / 3)
+      : (v) => operator[m.sign](v, m.b)
+  }))
+  let roundNr = 0
+  while (roundNr < x) {
+    roundNr++
+    monkeys.forEach(monkeyTurn)
 
-function runRounds (x) {
-  while(roundNr < x) {
-    runRound();
-    if ([1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, x].includes(roundNr)) {
-      console.log('After round', roundNr);
+    if (isLog && [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, x].includes(roundNr)) {
+      console.log('After round', roundNr)
       console.log(monkeys.map(m => m.itemsInspected))
     }
   }
+  const activity = monkeys.map(m => m.itemsInspected).sort(sortByNumberDesc)
+  return activity[0] * activity[1]
 }
-runRounds(700);
-// console.log(monkeys.map(m => ({ nr: m.itemsInspected, items: m.items })));
-const activity = monkeys.map(m => m.itemsInspected).sort(sortByNumberDesc);
-console.log('activity', activity)
-console.log('part2', activity[0] * activity[1])
 
-const elapsed = process.hrtime(start);
-
-console.log(`${elapsed[0]}s`, `${(elapsed[1] / 1000000).toFixed(3)}ms`)
+console.log('part1', runRounds(20, true))
+console.log('part2', runRounds(10000))
