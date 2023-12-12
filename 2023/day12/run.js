1e42afb88ab3f6d3fbd769bce3af64a4fd1d9431
Swaana <swaanas@gmail.com>
2023-12-12 21:41:26 +0100

    2023 day 12 (terrifyingly slow)

--- 2023/day12/run.js
+++ 2023/day12/run.js
@@ -1,4 +1,3 @@
-
 const readInput = require('../../utils/readInput');
 
 const input = readInput(__dirname, 'inputTst.txt');
@@ -6,30 +5,89 @@ const input = readInput(__dirname, 'inputTst.txt');
 const lines = input.split('\n').filter(l => !!l);
 
 // work per line, analyse and work out options
-lines.forEach((line) => {
+const rows = lines.map((line) => {
     const [springsPart, configPart] = line.split(' ');
-    const configs = configPart.split(',').map(Number);
-    const springs = springsPart.split('').map((s) => {
-        if (s === '.') {
-            return { damaged : false }
-        }
-        if (s === '#') {
-            return { damaged : true }
-        }
-        if (s === '?') {
-            return { unknown: true }
-        }
-    })
-    // find the groups of damaged
-    // or, just go from left to right and check options
-    // each ? is ether . or #
-    // each # is part of broken springs group
+    const config = configPart.split(',').map(Number);
 
-    // I think I have it, you walk forward and backtrack once you find an error
-
-
-    console.log(springs, configs)
+    return {
+        springs: springsPart.split(''),
+        config,
+    }
 })
 
-// console.log('part1', part1)
-// console.log('part2', part2)
+// console.log(rows)
+let found = [];
+function check(index, springs, config, damageLength, damageIndex) {
+    // console.log('checking', index, springs.join(''), damageLength, damageIndex, config)
+    if (index === springs.length) {
+        const dmgs = springs.join('').match(/#+/g)
+        if (!dmgs || dmgs.length !== config.length) {
+            return;
+        }
+        for(let i=0;i<dmgs.length;i++) {
+            if (dmgs[i].length !== config[i]) {
+                return;
+            }
+        }
+        found.push(springs.join(''))
+        // console.log('found', springs.join(''), springs.join('').match(/#+/g))
+        return;
+    }
+    const current = springs[index];
+
+    if (current === '#') {
+        if (damageLength && config[damageIndex] === damageLength) {
+            return;
+        }
+        check(index + 1, springs, config, damageLength + 1, damageIndex)
+    }
+
+    if (current === '.') {
+        if (damageLength && config[damageIndex] !== damageLength) {
+            return;
+        }
+        if (damageLength) {
+            check(index + 1, springs, config, 0, damageIndex + 1)
+        } else {
+            check(index + 1, springs, config, 0, damageIndex)
+        }
+    }
+
+    if (current === '?') {
+        // try it as # first, and then as .
+        if (config[damageIndex] > damageLength) {
+            springs[index] = '#';
+            check(index + 1, springs, config, damageLength + 1, damageIndex)
+        }
+        if (damageLength && config[damageIndex] !== damageLength) {
+            springs[index] = '?';
+            return;
+        }
+        springs[index] = '.';
+        if (damageLength) {
+            check(index + 1, springs, config, 0, damageIndex + 1)
+        } else {
+            check(index + 1, springs, config, 0, damageIndex)
+        }
+        springs[index] = '?';
+    }
+}
+
+rows.forEach(row => {
+    check(0, row.springs, row.config, 0, 0)
+})
+
+// console.log(found, found.length)
+const part1 = found.length;
+
+found = [];
+rows.forEach((row, i) => {
+    console.log('checking row', i)
+    check(0,
+        [...row.springs, '?', ...row.springs, '?', ...row.springs, '?', ...row.springs, '?', ...row.springs],
+        [...row.config, ...row.config, ...row.config, ...row.config, ...row.config], 0, 0)
+})
+
+const part2 = found.length;
+console.log('part1', part1)
+console.log('part2', part2)
